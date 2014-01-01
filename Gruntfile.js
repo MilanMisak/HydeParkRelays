094388e1e1c5f21dc1088c530fd00d778879b177
Milan Misak <misakmilan@gmail.com>
2014-01-01 20:42:39 +0100

    Installed grunt watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,10 +8,17 @@ module.exports = function(grunt) {
                     "assets/css/style.css": "assets/css/style.less"
                 }
             },
+        },
+        watch: {
+            scripts: {
+                files: '**/*.less',
+                tasks: ['less']
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('default', ['less']);
 
