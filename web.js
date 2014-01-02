3f0c281174aaecac80f75ff609c92399c043cfcc
Milan Misak <misakmilan@gmail.com>
2014-01-02 15:08:34 +0100

    Formatting change

--- web.js
+++ web.js
@@ -3,4 +3,6 @@ var express = require('express'),
 
 app.use('/', express.static(__dirname + '/assets'));
 
-app.listen(process.env.PORT || 3000, function() { console.log('listening')});
+app.listen(process.env.PORT || 3000, function() {
+    console.log('listening');
+});
