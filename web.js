e46b15218cb08f07b2b44b4b3b34975a514f6b1c
Milan Misak <misakmilan@gmail.com>
2014-01-02 15:24:55 +0100

    Added newrelic

--- web.js
+++ web.js
@@ -1,4 +1,5 @@
-var express = require('express'),
+var newrelic = require('newrelic'),
+    express = require('express'),
     app = express();
 
 app.use('/', express.static(__dirname + '/assets'));
