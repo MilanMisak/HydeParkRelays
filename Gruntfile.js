777ddb5897528672c0c0b1cda608a7f82117c457
Milan Misak <misakmilan@gmail.com>
2014-01-02 12:47:20 +0100

    Added a college map

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,24 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         less: {
+            options: {
+                compress: true
+            },
             development: {
                 files: {
-                    "assets/css/style.css": "assets/css/style.less"
+                    'assets/css/style.min.css': 'assets/css/style.less'
                 }
             },
+        },
+        uglify: {
+            development: {
+                options: {
+                    preserveComments: false
+                },
+                files: {
+                    'assets/js/scripts.min.js': ['assets/js/jquery-2.0.3.min.js', 'assets/js/jquery.swipebox.min.js', 'assets/js/index.js']
+                }
+            }
         },
         watch: {
             scripts: {
@@ -18,8 +31,9 @@ module.exports = function(grunt) {
     });
 
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['less']);
+    grunt.registerTask('default', ['less', 'uglify']);
 
 };
