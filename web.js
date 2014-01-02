131e0f4904d980873f6d005d89d74970d6de3d59
Milan Misak <misakmilan@gmail.com>
2014-01-02 21:06:30 +0100

    GZip compression

--- web.js
+++ web.js
@@ -2,6 +2,7 @@ var newrelic = require('newrelic'),
     express = require('express'),
     app = express();
 
+app.use(express.compress());
 app.use('/', express.static(__dirname + '/assets'));
 
 app.listen(process.env.PORT || 3000, function() {
