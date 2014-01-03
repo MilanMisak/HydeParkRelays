59a91dff737941b77aeebfe7181971b5b1e7f033
Milan Misak <misakmilan@gmail.com>
2014-01-03 13:19:41 +0100

    Setting expires headers

--- web.js
+++ web.js
@@ -2,6 +2,13 @@ var newrelic = require('newrelic'),
     express = require('express'),
     app = express();
 
+app.use(function(req, res, next){
+    if (req.url.match(/\.jpg$/)) {
+        res.setHeader("Cache-Control", "public, max-age=345600"); // 4 days
+        res.setHeader("Expires", new Date(Date.now() + 345600000).toUTCString());
+    }
+    next();
+});
 app.use(express.compress());
 app.use('/', express.static(__dirname + '/assets'));
 
